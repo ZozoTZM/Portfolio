82b7dafaf7f9271b4a82e2f27432dcc0f5c6acc8
ZozoTZM <torokzmark24@gmail.com>
2023-07-31 09:15:42 +0200

    Update Navbar.js

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -20,13 +20,13 @@ export default function Navbar() {
           <a href="#testimonials" className="mr-5 text-indigo-400 hover:text-white">
             Testimonials
           </a>
-        </nav>
+        </nav>      
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-blue-300 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Contact me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
+        </a>     
       </div>
     </header>
   );
