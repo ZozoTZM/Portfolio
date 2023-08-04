e4635de2147cc67cda64cdd840f59e2048742d68
ZozoTZM <torokzmark24@gmail.com>
2023-08-04 09:25:43 +0200

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,17 +5,31 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import CVPage from "./components/CVPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 export default function App() {
   return (
-    <main>
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CV" element={<CVPage />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+function Home() {
+  return (
+    <div>
       <Navbar />
       <About />
       <Projects />
       <Skills />
       <Testimonials />
       <Contact />
-    </main>
+    </div>
   );
 }
-
