54fb6daecf76536dd8e6e649de0c22db88c5be60
ZozoTZM <torokzmark24@gmail.com>
2023-08-04 09:25:51 +0200

    Update Navbar.js

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+        <div className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="/" className="ml-3 text-xl">
             Márk Török
           </a>
-        </a>
+        </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 text-indigo-400 hover:text-white">
             Past Work
@@ -19,6 +19,9 @@ export default function Navbar() {
           </a>
           <a href="#testimonials" className="mr-5 text-indigo-400 hover:text-white">
             Testimonials
+          </a>
+          <a href="/CV" className="mr-5 text-indigo-400 hover:text-white">
+            CV
           </a>
         </nav>      
         <a
