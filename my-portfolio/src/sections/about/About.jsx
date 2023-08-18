b9d0dd7aa4b6f8271a06b2f1a07ea7a7464d135e
ZozoTZM <torokzmark24@gmail.com>
2023-08-18 09:30:08 +0200

    Create About.jsx

---
+++ my-portfolio/src/sections/about/About.jsx
@@ -0,0 +1,43 @@
+import AboutImage from '../../assets/about.jpg'
+import CV from '../../assets/cv.pdf'
+import {HiDownload} from 'react-icons/hi'
+import data from './data'
+import Card from '../../components/Card'
+import './about.css'
+
+const About = () => {
+  return (
+    <section id="about" data-aos="fade-in">
+        <div className="container about__container">
+            <div className="about__left">
+                <div className="about__portrait">
+                    <img src={AboutImage} alt="About Image" />
+                </div>
+            </div>
+            <div className="about__right">
+                <h2>About Me</h2>
+                <div className="about__cards">
+                    {
+                        data.map(item => (
+                            <Card key={item.id} className="about__card">
+                                <span className='about__card-icon'>{item.icon}</span>
+                                <h5>{item.title}</h5>
+                                <small>{item.desc}</small>
+                            </Card>
+                        ))
+                    }
+                </div>
+                <p>
+                I'm easy going and patient. I have plenty of experience working within a team towards a common goal thanks to my hobbies since early teenage years (football, basketball, team based esport games). I like to help others but not afraid to ask for help either. I have a strong affinity towards everything IT, spend a lot of my time keeping up with news in the industry.
+                </p>
+                <p>
+                Hi, my name is Mark Török from Budapest, Hungary. I'm a full-stack web developer. Check out my resume below!
+                </p>
+                <a href={CV} download className='btn primary'>Download CV <HiDownload/></a>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
