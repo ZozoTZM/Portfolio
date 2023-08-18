e828fca427a5f5b267cc5fbad1459b54794a1216
ZozoTZM <torokzmark24@gmail.com>
2023-08-18 09:31:03 +0200

    Add data for experiences

---
+++ my-portfolio/src/sections/about/data.js
@@ -0,0 +1,13 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiHappyHeartEyes} from 'react-icons/bi'
+
+
+const data = [
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3+ Years Learning'},
+    {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '100+ Completed'} 
+    ]
+
+
+
+    export default data;
