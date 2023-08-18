da535134dff05e3c733534322c337ac36b01fb1d
ZozoTZM <torokzmark24@gmail.com>
2023-08-18 09:32:20 +0200

    Create data for projects display

---
+++ my-portfolio/src/sections/portfolio/data.js
@@ -0,0 +1,39 @@
+import Image1 from '../../assets/project1.jpg'
+import Image2 from '../../assets/project2.jpg'
+import Image3 from '../../assets/project3.jpg'
+import Image4 from '../../assets/project4.jpg'
+import Image5 from '../../assets/project5.jpg'
+import Image6 from '../../assets/project6.jpg'
+import Image7 from '../../assets/project7.jpg'
+import Image8 from '../../assets/project8.jpg'
+import Image9 from '../../assets/project9.jpg'
+
+const data = [   
+    {
+        id: 1,
+        category: 'frontend',
+        image: Image2,
+        title: "Remotivate",
+        desc: "Remotivate is a remote work management tool, similar to GitHub and Google Workspace. The main features include tracking projects and tasks, assigning members to them, and creating analytics. The site was developed using React frontend, .Net backend using Entity Framework and Identity Core, and was made by a group of 3.",       
+        github: 'https://github.com/Lukacs93/remote-work-management-tool'
+    },
+    {
+        id: 2,
+        category: 'frontend',
+        image: Image3,
+        title: "CoHotel",
+        desc: "CoHotel is an internal app made to track reservations for a Hotel. It uses React frontend with Asp.Net backend.",       
+        github: 'https://github.com/ZozoTZM/CoHotel'
+    },
+    {
+        id: 3,
+        category: 'frontend',
+        image: Image3,
+        title: "Portfolio",
+        desc: "A simple portfolio site made in React",       
+        github: 'https://github.com/ZozoTZM/Portfolio'
+    }
+]
+
+
+export default data
