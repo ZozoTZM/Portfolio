5c45112ed34a595571a116e5db20f182f5053a7b
ZozoTZM <torokzmark24@gmail.com>
2023-08-18 09:32:06 +0200

    Create data.js

---
+++ my-portfolio/src/sections/testimonials/data.js
@@ -0,0 +1,22 @@
+import Avatar1 from '../../assets/avatar1.jpg'
+import Avatar2 from '../../assets/avatar2.jpg'
+import Avatar3 from '../../assets/avatar3.jpg'
+import Avatar4 from '../../assets/avatar4.jpg'
+import Avatar5 from '../../assets/avatar5.jpg'
+import Avatar6 from '../../assets/avatar6.jpg'
+import Avatar7 from '../../assets/avatar7.jpg'
+
+
+const data = [
+        {
+        id: 1,
+        quote: "Zoltán has a solution-focused mindset, never losing sight of the goals he set for himself. Getting things done on time is a must for him, because he balances his academic and his Codecooler life simultaneously. His perseverance is commendable.",
+        avatar: Avatar1,
+        name: 'Kornél Regius',
+        profession: 'Codecool'
+        } 
+    ]
+
+
+
+    export default data
